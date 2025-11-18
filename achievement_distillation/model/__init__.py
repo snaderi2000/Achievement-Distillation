b37852838fb842bd89fdbbeb50369ecd72b11dd6
Shawheen Naderi <shawheennaderi@Shawheens-MacBook-Air.local>
2025-11-18 15:19:21 -0800

    changes to ppo_sd

--- achievement_distillation/model/__init__.py
+++ achievement_distillation/model/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseModel
 from .ppo import PPOModel
 from .ppo_ad import PPOADModel
-from .ppo_baseline import PPOBaselineModel
+from .ppo_baseline import PPOModelBaseline
 from .ppo_sd import PPOSDModel
